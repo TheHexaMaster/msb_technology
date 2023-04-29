b2c6631016cd6fbf16fe2ba4b492eebb61dc2542
TheHexaMaster <129121144+TheHexaMaster@users.noreply.github.com>
2023-04-29 18:23:28 +0200

    Update and rename setup_nets.sh to setup_ns.sh

--- setup_nets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo rm -v /etc/netplan/*
-echo 'network: \
-  version: 2 \
-  renderer: networkd \
-  ethernets: \
-    ens33: \
-      dhcp4: true' | sudo tee /etc/netplan/01-netcfg.yaml
---
+++ setup_ns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rm -v /etc/netplan/*
+echo 'network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    ens33:
+      dhcp4: true' | sudo tee /etc/netplan/01-netcfg.yaml
+sudo netplan generate
+sudo netplan apply
