1dc7c9c8be0a415848ee29ec29cddb7fb349b1ec
TheHexaMaster <129121144+TheHexaMaster@users.noreply.github.com>
2023-04-29 18:15:08 +0200

    Update setup_network.sh

--- setup_network.sh
+++ setup_network.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo mv /etc/netplan/*.yaml  /etc/netplan/01-netcfg.yaml
 echo 'network: \
   version: 2 \
   renderer: networkd \
   ethernets: \
     ens33: \
-      dhcp4: true' | sudo tee /etc/netplan/02-netcfg.yaml
+      dhcp4: true' | sudo tee /etc/netplan/01-netcfg.yaml
